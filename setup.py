f4421567978fa0cf36b651890d12d3d76f1b40d4
BD103 <bdeep10355@gmail.com>
2021-01-05 00:51:29 +0000

    Overall clean

--- setup.py
+++ setup.py
@@ -14,9 +14,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/BD103/PressPy",
     packages=setuptools.find_packages(),
+    install_requires=["click"],
+    entry_points={"console_scripts": ["presspy=presspy.cli:cli"]},
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Development Status :: 1 - Planning",
     ],
